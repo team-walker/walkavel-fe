a7053456b3b3b16526b0118dcb85e0ac54602004
Hayeon Baek <bhy0512@gmail.com>
2026-01-26 23:39:00 +0900

    chore: commitlint 대문자 규칙 비활성화

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0], // 0은 규칙을 비활성화(OFF) 함을 의미합니다.
+  },
+};
