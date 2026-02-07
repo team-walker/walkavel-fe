403e4c5c0d0ae240283974f39d11465a5ab162ba
Hayeon Baek <bhy0512@gmail.com>
2026-02-07 23:20:35 +0900

    feat: Orval을 이용한 API 타입 및 클라이언트 자동 생성

---
+++ orval.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  api: {
+    input: 'http://localhost:3001/docs/json',
+    output: {
+      mode: 'split',
+      target: './types/api.ts',
+      schemas: './types/model',
+      client: 'fetch',
+      prettier: true,
+    },
+  },
+});
