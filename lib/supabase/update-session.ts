e27db70dd021adddaf0dc19a828d93a5630b59a0
Hayeon Baek <bhy0512@gmail.com>
2026-02-09 01:19:07 +0900

    feat: Next.js 미들웨어 도입 및 특정 경로에 대한 서버 사이드 접근 제어 구현

---
+++ lib/supabase/update-session.ts
@@ -0,0 +1,50 @@
+import { createServerClient } from '@supabase/ssr';
+import { type NextRequest, NextResponse } from 'next/server';
+
+export async function updateSession(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
+          response = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // 보호된 경로 설정
+  if (
+    request.nextUrl.pathname.startsWith('/bookmark') ||
+    request.nextUrl.pathname.startsWith('/mypage')
+  ) {
+    if (!user) {
+      const url = request.nextUrl.clone();
+      url.pathname = '/login';
+      url.searchParams.set('returnTo', request.nextUrl.pathname);
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return response;
+}
