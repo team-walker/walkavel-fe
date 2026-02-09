297d8997742376e2d06f8d64a3c66685d534339c
Hayeon Baek <bhy0512@gmail.com>
2026-02-09 10:55:01 +0900

    refactor: 앱에 처음 진입했을 때만 스플래시 표시되도록 수정

--- store/splash.ts
+++ store/splash.ts
@@ -1,11 +1,27 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface SplashStore {
   isVisible: boolean;
+  _hasHydrated: boolean;
   hideSplash: () => void;
+  setHasHydrated: (state: boolean) => void;
 }
 
-export const useSplashStore = create<SplashStore>((set) => ({
-  isVisible: true,
-  hideSplash: () => set({ isVisible: false }),
-}));
+export const useSplashStore = create<SplashStore>()(
+  persist(
+    (set) => ({
+      isVisible: true,
+      _hasHydrated: false,
+      hideSplash: () => set({ isVisible: false }),
+      setHasHydrated: (state) => set({ _hasHydrated: state }),
+    }),
+    {
+      name: 'splash-storage',
+      storage: createJSONStorage(() => sessionStorage),
+      onRehydrateStorage: () => (state) => {
+        state?.setHasHydrated(true);
+      },
+    },
+  ),
+);
